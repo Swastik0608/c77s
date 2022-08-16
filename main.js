1260d112d494ef196aac75360481f51da435c0d2
Swastik0608 <102349946+Swastik0608@users.noreply.github.com>
2022-08-16 15:54:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log ("hi first console project");
+var first_name="Swastik";
+var last_name="Mittal";
+console.log (first_name.concat(last_name));
+var a=10;
+var b=5;
+var add=a + b;
+console.log(add)
+var subtract= a-b;
+console.log (subtract);
+var multiply=a*b;
+console.log(multiply);
+var divide= a/b;
+console.log(divide);
+
+
+
+
+
